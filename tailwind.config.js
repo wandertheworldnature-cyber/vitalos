4afaf426eb4e237162e14134637d1f2a2bc756ca
Appi <wandertheworldnature@gmail.com>
2026-05-04 15:34:59 +0530

    fix: restore Tailwind CSS directives - styles broken

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
     extend: {
-      screens: {
-        'xs': '375px',
-        'sm': '640px',
-        'md': '768px',
-        'lg': '1024px',
-        'xl': '1280px',
-      },
       colors: {
         brand: { DEFAULT: '#0f6e56', light: '#1d9e75', dark: '#0a5a46' }
       },
